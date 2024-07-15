d11b96cd90b43663b5c98f6e7bcfe0e2bc981cbf
dos-shaman <gmohan2803000@gmail.com>
2024-07-16 00:40:08 +0530

    refactor: clerk auth updated from deprecated code

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,3 @@
-import { authMiddleware } from "@clerk/nextjs/server";
-import { redirectToSignIn } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
-
-export default authMiddleware({
-  publicRoutes: ["/"],
-  afterAuth(auth,req){
-    if(auth.userId && auth.isPublicRoute){
-      let path = "/select-org";
-      if(auth.orgId){
-          path = `/organization/${auth.orgId}`;
-      }
-      
-      const orgSelection = new URL(path, req.url);
-      return NextResponse.redirect(orgSelection);
-    }
-    if(!auth.userId && !auth.isPublicRoute){
-      return redirectToSignIn({ returnBackUrl: req.url });
-    }
-    if(auth.userId && !auth.orgId && req.nextUrl.pathname !== "/select-org"){
-      const orgSelection = new URL("/select-org", req.url);
-      return NextResponse.redirect(orgSelection);
-    }
-  }
-});
 // import { clerkMiddleware, createRouteMatcher, redirectToSignIn } from '@clerk/nextjs/server'
 // import { NextResponse } from 'next/server';
 
@@ -54,6 +29,39 @@ export default authMiddleware({
 //   }
 // });
 
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const isProtectedRoute = createRouteMatcher([
+  '/organization(.*)',
+  '/board(.*)',
+  '/select-org',
+  '/api/cards(.*)'
+])
+
+
+
+export default clerkMiddleware((auth,req,event)=>{
+  if(!auth().userId && isProtectedRoute(req)){
+    return auth().redirectToSignIn({returnBackUrl:req.url})
+  } 
+  
+  if(auth().userId && !isProtectedRoute(req)){
+    let path : string = '/select-org'
+
+    if( auth().orgId){
+      path = `/organization/${auth().orgId}`
+    }
+    return NextResponse.redirect(new URL(path, req.url))
+  }
+
+  if(auth().userId && !auth().orgId && req.nextUrl.pathname !== "/select-org" ){
+    return NextResponse.redirect(new URL('/select-org', req.url))
+  }
+
+
+});
+
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-} 
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
