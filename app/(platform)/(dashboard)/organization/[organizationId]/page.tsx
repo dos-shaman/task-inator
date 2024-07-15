5b01f3d523dcdfb38cfdf462b8496bf575a34b92
dos-shaman <gmohan2803000@gmail.com>
2024-07-16 00:17:45 +0530

    feat: added zod validations

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,18 +1,29 @@
 import { db } from "@/lib/db";
 import { create } from "@/actions/create-board";
-const OrganizationIdPage = () => {
-
+import { Button } from "@/components/ui/button";
+const OrganizationIdPage = async () => {
+    const boards = await db.board.findMany();
     return (
-        <div>
+        <div className="flex flex-col space-y-4">
             <form action={create}>
                 <input
                     id="title"
                     name="title"
                     required
-                    placeholder="Well, let's get on it ya cunt!"
+                    placeholder="Oi! 'Omlanda nicked my son."
                     className="border border-black p-1" 
                 />
+                <Button type="submit">
+                    Find his son
+                </Button>
             </form>
+            <div className="space-y-2">
+                {boards.map((board) => (
+                    <div key={board.id}>
+                        Board title: {board.title}
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
